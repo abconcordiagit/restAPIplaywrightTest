801e5642bc22d36e85683d10c2d88bf8375f5830
abconcordiagit <abdullahconcordia@gmail.com>
2026-05-24 12:43:41 -0400

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+
+  testDir: './tests',
+
+  timeout: 30000,
+
+  retries: 1,
+
+  reporter: [
+    ['html'],
+    ['list']
+  ],
+
   use: {
     baseURL: 'https://gorest.co.in/public/v2'
   }
+
 });
