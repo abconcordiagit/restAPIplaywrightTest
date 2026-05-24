ddb06732a2b0b8e81f60e39988c1448118875a4f
abconcordiagit <abdullahconcordia@gmail.com>
2026-05-24 13:21:31 -0400

    update headers

--- utils/headers.ts
+++ utils/headers.ts
@@ -1,4 +1,12 @@
-export const headers = {
-  Authorization: `Bearer ${process.env.GOREST_TOKEN}`,
-  'Content-Type': 'application/json'
-};
+export function getHeaders() {
+  const token = process.env.GOREST_TOKEN;
+
+  if (!token) {
+    throw new Error("❌ GOREST_TOKEN is missing in environment variables");
+  }
+
+  return {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json'
+  };
+}
