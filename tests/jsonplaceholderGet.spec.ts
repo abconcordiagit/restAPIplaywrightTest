563268efecd3dc13d57eace417445ab3761baaeb
abconcordiagit <abdullahconcordia@gmail.com>
2026-05-24 19:32:07 -0400

    https://jsonplaceholder.typicode.com/users simple get request

---
+++ tests/jsonplaceholderGet.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('GET users', async ({ request }) => {
+
+  const response = await request.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+
+  expect(response.status()).toBe(200);
+
+  const body = await response.json();
+
+  expect(body.length).toBeGreaterThan(0);
+
+  console.log(body[0]);
+
+});
