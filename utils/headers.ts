222342082ddcb029993d136e202fb19fd3817f69
abconcordiagit <abdullahconcordia@gmail.com>
2026-05-24 12:40:04 -0400

    update headers

--- utils/headers.ts
+++ utils/headers.ts
@@ -1,4 +1,4 @@
 export const headers = {
-  Authorization: `Bearer ee57b42a89498e463ef3a0a44358049fa2cc7c9a53e9d2daa2f5b1e19da5dc91`,
+  Authorization: `Bearer ${process.env.GOREST_TOKEN}`,
   'Content-Type': 'application/json'
 };
